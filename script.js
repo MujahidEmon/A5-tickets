cc14a12b31be6cd8d2968bba2be7567bf25ed6b1
MujahidEmon <mujahidulislam.official@gmail.com>
2024-02-19 18:47:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// const seats = document.getElementsByClassName("seat");
+// for (let seat of seats) {
+//   seat.addEventListener("click", function () {
+//     seat.style.backgroundColor = "#1DD100";
+//     seat.style.color = "#fff";
+//   });
+// }
